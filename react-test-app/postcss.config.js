e39e051aa171633d986c4cf86e28c7337689ecd9
MOJOEA <DevineTYpor@gmail.com>
2026-04-27 16:46:34 +0700

    setTailwind

---
+++ react-test-app/postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {}, // เปลี่ยนเป็นตัวนี้
+    autoprefixer: {},
+  },
+}
---
+++ react-test-app/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // ตรวจสอบว่ามีบรรทัดนี้ และสะกดถูกต้อง
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
