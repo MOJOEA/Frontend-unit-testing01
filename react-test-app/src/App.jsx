907bbfe08d763891415aaf91f2b182e905fb38ae
MOJOEA <DevineTYpor@gmail.com>
2026-04-27 16:28:06 +0700

    UserList.test

--- react-test-app/src/App.jsx
+++ react-test-app/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-import Counter from './components/Counter/Counter';
+// import Counter from './components/Counter/Counter';
+import UserList from './components/UserList/UserList';
 
 function App() {
   return (
     <>
     <h1>React Test App</h1>
-    <Counter />
+    <UserList />
     </>
   )
 }
